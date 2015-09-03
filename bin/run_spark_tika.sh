f304f73433608cc2457b60f7efe1e7f206ab75d7
Scott Haleen <scotthaleen@users.noreply.github.com>
2015-09-03 15:17:46 -0400

    Update run_spark_tika.sh

--- bin/run_spark_tika.sh
+++ bin/run_spark_tika.sh
@@ -6,4 +6,4 @@ if [[ -d "pst-extract/example/spark-attach" ]]; then
     rm -rf "pst-extract/example/spark-attach"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --jars lib/tika-app-1.10.jar --conf spark.storage.memoryFraction=.8 --class newman.Driver lib/tika-extract_2.10-1.0.0.jar pst-extract/example/pst-json/ pst-extract/example/spark-attach etc/exts.txt 
+spark-submit --master local[*] --driver-memory 8g --jars lib/tika-app-1.10.jar --conf spark.storage.memoryFraction=.8 --class newman.Driver lib/tika-extract_2.10-1.0.0.jar pst-extract/pst-json/ pst-extract/spark-attach etc/exts.txt 
