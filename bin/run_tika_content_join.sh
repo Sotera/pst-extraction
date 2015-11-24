908e010fda57a93e951afbb5cbbc4bb454d20bed
U-PFI\cschulze <cschulze@schulze-pc.PFI.LOCAL>
2015-11-23 21:15:15 -0600

    Changed run_tika_content_join.sh to as to align attachments with spam filtered output

--- bin/run_tika_content_join.sh
+++ bin/run_tika_content_join.sh
@@ -6,4 +6,4 @@ if [[ -d "pst-extract/spark-emails-attach" ]]; then
     rm -rf "pst-extract/spark-emails-attach"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 spark/attachment_join.py pst-extract/pst-json/ pst-extract/spark-attach/ pst-extract/spark-emails-attach
+spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 spark/attachment_join.py pst-extract/post-spam-filter/ pst-extract/spark-attach/ pst-extract/spark-emails-attach
