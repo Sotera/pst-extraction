490dfd85cc28addc63a9c3703cfc7ce27d6163a7
U-PFI\cschulze <cschulze@schulze-pc.PFI.LOCAL>
2015-12-01 15:30:33 -0600

    Fixed hardcoding of source destination of bin/run_spam_filter.sh command

--- bin/run_spam_filter.sh
+++ bin/run_spam_filter.sh
@@ -6,4 +6,4 @@ if [[ -d "pst-extract/post-spam-filter" ]]; then
     rm -rf "pst-extract/post-spam-filter"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/naive_bayes_classifier.pkl spark/spam_filter_harness.py /vagrant/pst-extraction/pst-extract/pst-json /vagrant/pst-extraction/pst-extract/post-spam-filter
+spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/naive_bayes_classifier.pkl spark/spam_filter_harness.py pst-extract/pst-json pst-extract/post-spam-filter
