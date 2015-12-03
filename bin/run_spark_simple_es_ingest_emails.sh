c190b5280725bea035bea7b34a14da3894dceea0
Elliot Ickovic <elliot.ickovic@gmail.com>
2015-12-03 16:51:13 -0500

    Added a simplistic non ES/ Hadoop way to ingest into ES.  This is due to bugs incountered while running on the cluster which prevented ingest. Added scripts to drive the process.

---
+++ bin/run_spark_simple_es_ingest_emails.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set +x
+set -e
+
+if [[ $# -lt 1 ]]; then
+    printf "missing configuration\n"
+    exit 1
+fi
+
+source $1
+
+
+response=$(curl -XHEAD -i --write-out %{http_code} --silent --output /dev/null "${ES_HOST}:${ES_PORT}/${ES_INDEX}")
+
+if [[ "$response" -eq 404 ]]; then
+    printf "create index ${ES_INDEX}\n"
+    curl -s -XPUT "${ES_HOST}:${ES_PORT}/${ES_INDEX}" --data-binary "@etc/settings.json"    
+fi
+
+response=$(curl -XHEAD -i --write-out %{http_code} --silent --output /dev/null "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_EMAILS}")
+if [[ "$response" -eq 200 ]]; then
+    printf "delete doc_type\n"
+    curl -XDELETE "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_EMAILS}"
+fi
+
+printf "create emails doc_type\n"
+curl -s -XPUT "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_EMAILS}/_mapping" --data-binary "@etc/emails.mapping"
+
+
+response=$(curl -XHEAD -i --write-out %{http_code} --silent --output /dev/null "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_CLUSTERING}")
+if [[ "$response" -eq 200 ]]; then
+    printf "delete doc_type\n"
+    curl -XDELETE "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_CLUSTERING}"
+fi
+
+printf "create lda-clustering doc_type\n"
+curl -s -XPUT "${ES_HOST}:${ES_PORT}/${ES_INDEX}/${ES_DOC_TYPE_CLUSTERING}/_mapping" --data-binary "@etc/lda-clustering.mapping"
+
+printf "ingest lda clusters\n"
+./src/upload_lda_clusters.py ${ES_INDEX} --es_nodes ${ES_NODES}
+
+printf "ingest entity documents\n"
+
+spark-submit --master local[*] --driver-memory 8g --jars lib/elasticsearch-hadoop-2.2.0-m1.jar --conf spark.storage.memoryFraction=.8 spark/es_simple_ingest.py "pst-extract/spark-emails-entity/part-*" "${ES_INDEX}/${ES_DOC_TYPE_EMAILS}" --id_field id  --es_nodes ${ES_NODES}
