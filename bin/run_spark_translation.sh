306a9b718a3e1db975698daed9739c1ddeefbeb7
Elliot Ickovic <elliot.ickovic@gmail.com>
2016-03-23 14:36:08 -0400

    Allow toggle of moses and language

--- bin/run_spark_translation.sh
+++ bin/run_spark_translation.sh
@@ -6,4 +6,4 @@ if [[ -d "pst-extract/spark-emails-translation" ]]; then
     rm -rf "pst-extract/spark-emails-translation"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 spark/language.py pst-extract/spark-emails-with-topics pst-extract/spark-emails-translation
+spark-submit --master local[*] --driver-memory 8g --files spark/moses_translator.py --conf spark.storage.memoryFraction=.8 spark/translation.py pst-extract/spark-emails-with-topics pst-extract/spark-emails-translation --force_language en --translation_mode apertium --moses_server localhost:8080
