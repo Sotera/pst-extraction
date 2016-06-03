e0310c463aaf626b9788e211aae4055708dcc0c0
Robert Parkhurst <raparkhurst@digitalsynapse.io>
2016-06-03 13:29:14 -0400

    updated newman_pipeline.py to add support for logging and some error handling

--- bin/newman_pipeline.py
+++ bin/newman_pipeline.py
@@ -49,28 +49,26 @@ if options.newman_csv_file is None:
     sys.exit("Error:  No input file given, cannot continue")
 
 # Read in the config file as csv
-data={}
-with open(str(options.newman_csv_file)) as fin:
-    reader=csv.reader(fin, skipinitialspace=True, quotechar="'")
-    for row in reader:
-        data[row[0]]=row[1:]
+with open(str(options.newman_csv_file), 'r') as fin:
+    reader = csv.reader(fin)
+    data = list(reader)
 
 # we want to make sure we were able to get the information
 if options.debug is True:
-    pp = pprint.PrettyPrinter(width=41, compact=True)
+    pp = pprint.PrettyPrinter(compact=True)
     pp.pprint(data)
 else:
     print("data file read in.  Ready for processing")
 
 
-for key, value in data.items():
-    filename = str(key)
-    pipeline_name = str(value[0])
-    case_id = str(value[1])
-    reference_label = str(value[2])
-    label = str(value[3])
-    language = str(value[4])
-    mbox_type = str(value[5])
+for item in data:
+    filename = str(item[0])
+    pipeline_name = str(item[1])
+    case_id = str(item[2])
+    reference_label = str(item[3])
+    label = str(item[4])
+    language = str(item[5])
+    mbox_type = str(item[6])
 
     split_filename = filename.split('/')
 
@@ -84,6 +82,8 @@ for key, value in data.items():
         dest_filename = split_filename[-1]
 
     if options.debug is True:
+        print("")
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print("key (filename) is:  " + filename)
         print("\tpipeline name:  " + pipeline_name)
         print("\tcase_id is:  " + case_id)
@@ -92,23 +92,55 @@ for key, value in data.items():
         print("\tlanguage is:  " + language)
         print("\tmailbox type is:  " + mbox_type)
         print("\tdest_filename is:  " + dest_filename)
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        print("")
 
     # clean up from any possible previous run
     if options.test_mode is False:
         exitcode = subprocess.call(["sudo rm -fr /srv/software/pst-extraction-master/pst-extract/*"], shell=True)
-        os.mkdir(newman_process_destination + "/pst")
-        os.mkdir(newman_process_destination + "/mbox")
+        try:
+            os.mkdir(newman_process_destination + "/pst")
+            os.mkdir(newman_process_destination + "/mbox")
+        except:
+            print("Error, unable to create directories for pst/mbox processing")
+            print("Please make sure you have access to create directories under:  " + newman_process_destination)
+            exit(0)
+
+        try:
+            os.mkdir("/var/log/newman/")
+        except:
+            print("Error creating /var/log/newman/ for logging.")
+            print("Please make sure you have access to create this.")
+            print("Error Message:  ", sys.exc_info()[0])
+            exit(0)
+
 
         run_command = pipeline_name + " " + case_id + " " + reference_label + " " + label + " " + language
+        mbox_log_file = "/var/log/newman" + dest_filename + ".log"
+
+        if options.debug is True:
+            print("")
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            print("run command is:  " + run_command)
+            print("mailbox log file is:  ", mbox_log_file)
+            print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            print("")
+
+        print("\n\n")
+        print("Processing:  " + dest_filename + "\n")
+
         if mbox_type == "pst":
             cp_command = ["cp -R " + str(key) + " " + newman_process_destination + "/pst/" + dest_filename]
             exitcode = subprocess.call(cp_command, shell=True)
-            exitcode = subprocess.call(["./bin/pst_all.sh " + run_command.lower()], shell=True)
+            exitcode = subprocess.call(["./bin/pst_all.sh " + run_command.lower() + " 2>&1 | tee " + mbox_log_file], shell=True)
         elif mbox_type == "mbox":
             cp_command = ["cp -R " + str(key) + " " + newman_process_destination + "/mbox/" + dest_filename]
             exitcode = subprocess.call(cp_command, shell=True)
-            exitcode = subprocess.call(["./bin/mbox_all.sh " + run_command.lower()], shell=True)
+            exitcode = subprocess.call(["./bin/mbox_all.sh " + run_command.lower() + " 2>&1 | tee " + mbox_log_file], shell=True)
         else:
             sys.exit("Error:  No type given, must provide pst or mbox.  Use -h to see help")
+
+        print("\n\n")
+        print("Done with: " + dest_filename + "\n")
 
 print("done.")
