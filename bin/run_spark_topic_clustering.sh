0afc6363f095cf0445d1c414af1b5602c1dc8e61
Jakob Lee <simplexity.product@gmail.com>
2017-02-10 21:53:52 -0500

    updated default configs for tocpic-clustering process start script

--- bin/run_spark_topic_clustering.sh
+++ bin/run_spark_topic_clustering.sh
@@ -29,10 +29,10 @@ if [[ -d "pst-extract/$OUTPUT_DIR" ]]; then
     rm -rf "pst-extract/$OUTPUT_DIR"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py spark/topic_clustering.py --stopwords etc/english.stopwords --vocab_index tmp/vocab.idx pst-extract/spark-emails-with-communities pst-extract/spark-lda-input ${RUN_FLAGS}
+spark-submit --master local[1] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py spark/topic_clustering.py --stopwords etc/english.stopwords --vocab_index tmp/vocab.idx pst-extract/spark-emails-with-communities pst-extract/spark-lda-input ${RUN_FLAGS}
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --class newman.Driver lib/newman-lda-topics_2.10-1.0.0.jar "pst-extract/spark-lda-input/part-*" "pst-extract/spark-lda-results" "tmp/vocab.idx" "tmp/lda.map.txt"
+spark-submit --master local[1] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --class newman.Driver lib/newman-lda-topics_2.10-1.0.0.jar "pst-extract/spark-lda-input/part-*" "pst-extract/spark-lda-results" "tmp/vocab.idx" "tmp/lda.map.txt"
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py spark/lda_merge.py pst-extract/spark-emails-with-communities pst-extract/spark-lda-results pst-extract/$OUTPUT_DIR ${RUN_FLAGS}
+spark-submit --master local[1] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py spark/lda_merge.py pst-extract/spark-emails-with-communities pst-extract/spark-lda-results pst-extract/$OUTPUT_DIR ${RUN_FLAGS}
 
 ./bin/validate_lfs.sh $OUTPUT_DIR
