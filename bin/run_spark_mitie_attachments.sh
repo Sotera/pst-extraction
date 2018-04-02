62eab53dc99052bcf57caef93a4af2b9b6970373
jstnldrs <justin.lueders@gmail.com>
2018-04-02 14:19:46 -0500

    adding spacy

--- bin/run_spark_mitie_attachments.sh
+++ bin/run_spark_mitie_attachments.sh
@@ -8,4 +8,6 @@ if [[ -d "pst-extract/spark-attachments_entities" ]]; then
     rm -rf "pst-extract/spark-attachments_entities"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files mitie.py,libmitie.so,ner_model.dat spark/mitie_entity_ingest_file.py pst-extract/spark-emails-attachments pst-extract/spark-attachments_entities --extract_field content
+spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files mitie.py,libmitie.so,ner_model.dat spark/spacy_entity_ingest_file.py pst-extract/spark-emails-attachments pst-extract/spark-attachments_entities --extract_field content
+
+#spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files mitie.py,libmitie.so,ner_model.dat spark/mitie_entity_ingest_file.py pst-extract/spark-emails-attachments pst-extract/spark-attachments_entities --extract_field content
