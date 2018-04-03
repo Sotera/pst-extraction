de53fd1a7da7acda262cedcabf4a7eb604eb9369
jstnldrs <justin.lueders@gmail.com>
2018-04-03 09:54:12 -0500

    convert to spacy for entity extraction

--- bin/run_spark_mitie.sh
+++ bin/run_spark_mitie.sh
@@ -12,6 +12,7 @@ if [[ -d "pst-extract/$OUTPUT_DIR" ]]; then
     rm -rf "pst-extract/$OUTPUT_DIR"
 fi
 
-spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py,mitie.py,libmitie.so,ner_model_english.dat,ner_model_spanish.dat spark/mitie_entity_ingest_file.py pst-extract/spark-emails-translation pst-extract/$OUTPUT_DIR --extract_field body ${RUN_FLAGS}
+spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py,mitie.py,libmitie.so,ner_model_english.dat,ner_model_spanish.dat spark/spacy_entity_ingest_file.py pst-extract/spark-emails-translation pst-extract/$OUTPUT_DIR --extract_field body ${RUN_FLAGS}
+#spark-submit --master local[*] --driver-memory 8g --conf spark.storage.memoryFraction=.8 --files spark/filters.py,mitie.py,libmitie.so,ner_model_english.dat,ner_model_spanish.dat spark/mitie_entity_ingest_file.py pst-extract/spark-emails-translation pst-extract/$OUTPUT_DIR --extract_field body ${RUN_FLAGS}
 
 ./bin/validate_lfs.sh $OUTPUT_DIR
